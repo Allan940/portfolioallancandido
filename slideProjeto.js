74731313accd08332035518e33eb014dbfb81594
Allan Cândido <65836765+Allan940@users.noreply.github.com>
2020-11-01 12:57:05 -0300

    Add files via upload

---
+++ slideProjeto.js
@@ -0,0 +1,35 @@
+$(function(){
+	slideProjeto();
+	function slideProjeto(){
+		$('.slide-wraper').slick({
+			infinite:true,
+			centerMode:true,
+			slidesToShow:4,
+			arrows:false,
+			responsive:[
+
+
+            {
+            	breakpoint:768,
+            	 settings:{
+            		slidesToShow:3,
+            		centerMode:false,
+            		arrows:false,
+            		infinite:true
+            	}
+            },
+            {
+            	breakpoint:500,
+            	settings:{
+            		slidesToShow:1,
+            		centerMode:true,
+            		arrows:false,
+            		infinite:true
+
+            	}
+            }
+
+			]
+		});
+	}
+})
